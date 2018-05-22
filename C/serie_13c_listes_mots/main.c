4879d2713ac457b53af9709082b267de3f779ae2
Evlyn <evlyn@idlab.ch>
2018-05-22 20:42:23 +0200

    Avancement 13C

--- C/serie_13c_listes_mots/main.c
+++ C/serie_13c_listes_mots/main.c
@@ -55,16 +55,58 @@ char * clean(char * mot) {
     return mot;
 }
 
-//// Insère un mot dans une liste triée lexicographiquement et incrémente le compteur si le mot est déjà présent
-//void insert() {
-//
-//}
-//
 //// Recherche un mot dans une liste triée et retourne le nombre d'occurences de ce mot
 //void search() {
 //
 //}
 //
+
+// Insère un mot dans une liste triée lexicographiquement et incrémente le compteur si le mot est déjà présent
+void insert(char * mot, ListeMot * liste) {
+
+    // Création d'un objet "mot"
+    Mot *motToInsert = malloc(sizeof(Mot));
+    motToInsert->mot = mot;
+
+    // Si la chaîne est vide, on insère directement le mot
+    if(liste->premier == NULL) {
+        liste->premier = motToInsert;
+        motToInsert->nbOccurence = 0;
+        liste->nbMot = 1;
+    } else {
+
+        // Rechercher le mot dans la liste. S'il y est : le trouver et augmenter son nb d'occurences de 1 ; sinon, trouver où le positionner
+        int * nbOccurence = search(motToInsert);
+        if(nbOccurence != NULL) {
+            nbOccurence++;
+        } else {
+
+            motToInsert->nbOccurence = 0;
+
+            // Parcourir la liste pour voir où mettre notre chaine
+
+            Mot *motToCompare = malloc(sizeof(Mot));
+            motToCompare = liste->premier;
+
+            if(strcmp(motToCompare->mot, motToInsert->mot) < 0 && strcmp(motToInsert->mot, motToCompare->suivant->mot) < 0) {
+                liste->premier = motToInsert;
+                motToCompare->precedent = motToInsert;
+            } else {
+
+                for(int j = 0; j < liste->nbMot; j++) {
+                    if(strcmp(motToCompare->mot, motToInsert->mot) < 0 && strcmp(motToInsert->mot, motToCompare->suivant->mot) < 0) {
+                        // motToInsert
+
+                        
+
+                    }
+                }
+
+            }
+        }
+    }
+}
+
 //// Supprime un mot dans une liste triée
 //void delete() {
 //
@@ -104,10 +146,11 @@ int main() {
         while (!feof(fid)) {
             fscanf(fid, "%s", mot);
 
-            Mot *motToInsert = malloc(sizeof(Mot));
-
             // Nettoyage des caractères de ponctuation et mise en minuscules
             motCleane = clean(mot);
+
+            // Insérer le mot dans la liste
+            insert(motCleane, liste);
         }
         fclose(fid); // fermeture du fichier
     } else {
